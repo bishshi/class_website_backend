c42759896efce46f4bd890440a43b0b7f91c6de9
bisnsh <bishsh2006@outlook.com>
2026-01-23 09:11:10 +0800

    update server config to enable cron jobs v1.5.0

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,27 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  // ..
+	'publisher': {
+		enabled: true,
+		config: {
+				hooks: {
+					beforePublish: async ({ strapi, uid, entity }) => {
+						// Return false to prevent publish; any other value (or no return) allows it
+						console.log('beforePublish');
+						// return false
+					},
+					afterPublish: async ({ strapi, uid, entity }) => {
+						console.log('afterPublish');
+					},
+					beforeUnpublish: async ({ strapi, uid, entity }) => {
+						// Return false to prevent unpublish; any other value (or no return) allows it
+						console.log('beforeUnpublish');
+						// return false
+					},
+					afterUnpublish: async ({ strapi, uid, entity }) => {
+						console.log('afterUnpublish');
+					},
+				},
+		},
+	},
+	// ..
+});
