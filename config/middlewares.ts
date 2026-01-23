5b957f21247ceafe5484d09d1e735f23304c6083
bisnsh <bishsh2006@outlook.com>
2026-01-23 13:20:01 +0800

    fix reaction time middleware to handle async functions properly

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,31 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://class.biss.click',        // 你的前端域名
+        'https://cms.class.biss.click',    // 你的 CMS 域名
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+        'X-Reactions-Author',  // ← 关键: 添加这个自定义头部
+      ],
+      exposedHeaders: ['Content-Range', 'X-Content-Range'],
+      credentials: true,
+      maxAge: 3600,
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
